b105757ee3f0eb3eb6314d13d415ca76e80f4476
Sam Magura <srmagura@gmail.com>
2021-12-22 12:26:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# rtk-add-matcher-repro
+
+1. `yarn install`
+2. Open `src/index.ts` in an editor that has TypeScript intellisense.
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+interface SpecialAction {
+  // Uncomment the next line, and then action.payload will be typed correctly
+  // type: string;
+  payload: number;
+}
+
+function isSpecialAction(action: any): action is SpecialAction {
+  return action.type.startsWith("special/");
+}
+
+const myReducer = createReducer(0, (builder) => {
+  builder.addMatcher(isSpecialAction, (state, action) => {
+    // Hover over `action` to see that its type is `AnyAction`, not `SpecialAction`
+    return action.payload;
+  });
+});
